c601fd3927c2b6d1f6e0efcf480a80a4eae2762d
Skeyelab <eric@skeyelab.com>
2025-08-24 03:13:59 -0400

    style: fix StandardRB violations in database_cleaner.rb
    - Convert single quotes to double quotes for consistency
    - Ensures all code passes StandardRB linting

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -6,17 +6,17 @@ RSpec.configure do |config|
   # Only clean database when absolutely necessary
   config.before(:suite) do
     # Clean database once at start
-    ActiveRecord::Base.connection.execute('TRUNCATE TABLE trading_pairs RESTART IDENTITY CASCADE;')
-    ActiveRecord::Base.connection.execute('TRUNCATE TABLE candles RESTART IDENTITY CASCADE;')
-    ActiveRecord::Base.connection.execute('TRUNCATE TABLE sentiment_events RESTART IDENTITY CASCADE;')
-    ActiveRecord::Base.connection.execute('TRUNCATE TABLE sentiment_aggregates RESTART IDENTITY CASCADE;')
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE trading_pairs RESTART IDENTITY CASCADE;")
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE candles RESTART IDENTITY CASCADE;")
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentiment_events RESTART IDENTITY CASCADE;")
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE sentiment_aggregates RESTART IDENTITY CASCADE;")
   end
 
   # Use fast truncation only for tests that need it
   config.before(:each, :database_cleanup) do
-    ActiveRecord::Base.connection.execute('DELETE FROM trading_pairs;')
-    ActiveRecord::Base.connection.execute('DELETE FROM candles;')
-    ActiveRecord::Base.connection.execute('DELETE FROM sentiment_events;')
-    ActiveRecord::Base.connection.execute('DELETE FROM sentiment_aggregates;')
+    ActiveRecord::Base.connection.execute("DELETE FROM trading_pairs;")
+    ActiveRecord::Base.connection.execute("DELETE FROM candles;")
+    ActiveRecord::Base.connection.execute("DELETE FROM sentiment_events;")
+    ActiveRecord::Base.connection.execute("DELETE FROM sentiment_aggregates;")
   end
 end
